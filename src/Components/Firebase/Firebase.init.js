072be1de7b4a326a524434f55f755c228a136da3
sampod76 <108483839+sampod76@users.noreply.github.com>
2022-10-25 10:09:32 +0600

    add firebase and creaite privet route

---
+++ src/Components/Firebase/Firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAOVnhnia2ZfOeYeo3MuM1lQmQ5krMgWNQ",
+  authDomain: "assernment-10.firebaseapp.com",
+  projectId: "assernment-10",
+  storageBucket: "assernment-10.appspot.com",
+  messagingSenderId: "761976794110",
+  appId: "1:761976794110:web:8deec64f52990d113baeaf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
