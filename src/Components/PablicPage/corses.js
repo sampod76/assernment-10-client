8d8884555df8ecf79fa08fe7178b54ac5b3de9e2
Amrita Chandra Das <sampodnath76@gmail.com>
2022-10-26 16:34:50 +0600

    corses to make card and show data

--- src/Components/PablicPage/corses.js
+++ src/Components/PablicPage/corses.js
@@ -11,7 +11,11 @@ const Corses = () => {
         <div className="max-w-screen-xl p-5 mx-auto dark:bg-gray-800 dark:text-gray-100">
             <div className="grid grid-cols-1 gap-5 lg:grid-cols-4 sm:grid-cols-2">
 
-               
+                {
+                    getCoures.map(cor => <Card cor={cor} key={cor.id}>
+                        
+                    </Card>)
+                }
 
             </div>
         </div>
